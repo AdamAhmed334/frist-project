f3f2ac91f912e3b0f047025a4b546212b7891bd5
AdamAhmed334 <jdd28675@gmail.com>
2026-05-13 05:57:09 -0700

    Delete dd.py

--- dd.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from groq import Groq
-st.title("Adamh_app")
-cilent = Groq(api_key=st.secrets["qroc_api-key"])
-
-text = st.text_area("تلخيص ما كتبت",hight = 2000)
-if st.button("لخص"):
-  if len(text.splite()) < 10:
-    st.wraning("النص قصير اكتب نص اكبر")
