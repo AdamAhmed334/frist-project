8a278c9e3ef4c1611ebd13dc449fbfd1d05e3234
AdamAhmed334 <jdd28675@gmail.com>
2026-05-30 12:32:59 -0700

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from groq import Groq
+st.title("Adamh_app")
+cilent = Groq(api_key=st.secrets["qroc_api_key"])
+
+text = st.text_area("تلخيص ما كتبت",hight = 2000)
+if st.button("لخص"):
+  if len(text.splite()) < 10:
+    st.wraning("النص قصير اكتب نص اكبر")
+else:
+    with st.spinner("جار تلخيص"):
+      arcbic_letter = 0
+      for c in text:
+        if '\u0600' <= c <= '\u06FF':
+          arcbic_letter+= 1
+      if arcbic_letter > 12:
+        language = "العربية"
+      else:
+        language = "English"
+      chatbot = cilent.chat.completions.create(
+        model = "llama-3.3-70b-versatile",  
+        messages = [{"role":"system","content":f"You are a helpful assistant for kid. Summarize the text in 3-4 sentences. You MUST respond in {language} only"},
+                   {"role":"user","content":text}]
+      )
+      st.success(chatbot.choices[0].message.content)
